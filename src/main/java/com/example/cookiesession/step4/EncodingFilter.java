78c756b84f403784c9d09b6fb4dbebfdd18d61bf
a1l1ke <onetenthplease@gmail.com>
2026-07-03 12:36:58 +0900

    feat: 필터 기능 구현을 위한 EncodingFilter 및 FilterServlet 추가

---
+++ src/main/java/com/example/cookiesession/step4/EncodingFilter.java
@@ -0,0 +1,33 @@
+package com.example.cookiesession.step4;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+// 1. @WebFilter -> 패턴으로 사용하는게 권장
+// 2. implements
+@WebFilter("/*") // / -> 접두사로하는 요청을 다 받아주겠다
+//@WebFilter({"/filter/*", "/session/*"})
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("EncodingFilter.init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("EncodingFilter.doFilter");
+        // ServletRequest -> HttpServletRequest
+        HttpServletRequest req = (HttpServletRequest) request; // 다운캐스팅
+        System.out.println("RequestURI = " + req.getRequestURI());
+        chain.doFilter(request, response); // chain을 다음으로 넘겨주지 않으면 원래 가려던 곳으로 못감
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("EncodingFilter.destroy");
+    }
+}
